9f647b092a9ebadf04a37bd04aa172d00e9947f4
Bradley Wojcik <bradleywojcik@gmail.com>
2019-05-05 19:51:17 -0400

    added another nose test for app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,3 +4,13 @@ def test_win_probability():
     prob1, prob2 = app.win_probability(1000.0, 1000.0)
     assert prob1 == 0.5
     assert prob2 == 0.5
+
+def test_score_weight():
+    weight1 = app.score_weight(1, 1)
+    assert weight1 == 1
+
+    weight2 = app.score_weight(3, 1)
+    assert weight2 == 1.25
+
+    weight3 = app.score_weight(5, 1)
+    assert weight3 == 1.5
